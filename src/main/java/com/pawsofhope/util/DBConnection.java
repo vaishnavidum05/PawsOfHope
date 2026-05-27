9963ce38503bfea41c974dd378feb9c177c689f6
vaishnavi <vaishnavi_dum_comp@moderncoe.edu.in>
2026-05-27 11:09:41 +0530

    Hardcoded Railway MySQL connection

--- src/main/java/com/pawsofhope/util/DBConnection.java
+++ src/main/java/com/pawsofhope/util/DBConnection.java
@@ -10,22 +10,11 @@ public class DBConnection {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            String host = System.getenv("MYSQLHOST");
-            String port = System.getenv("MYSQLPORT");
-            String database = System.getenv("MYSQLDATABASE");
-            String user = System.getenv("MYSQLUSER");
-            String password = System.getenv("MYSQLPASSWORD");
-
-            if (host == null) {
-                return DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/pawsofhope",
-                        "root",
-                        "Golden@1640"
-                );
-            }
-
-            String url = "jdbc:mysql://" + host + ":" + port + "/" + database +
+            String url = "jdbc:mysql://kodama.proxy.rlwy.net:40540/railway" +
                     "?useSSL=false&allowPublicKeyRetrieval=true";
+            String user = "root";
+            String password = "uljndBGVIFlWTQyalKkRfuoEnUTnVvOl";
+
             return DriverManager.getConnection(url, user, password);
 
         } catch (ClassNotFoundException e) {
